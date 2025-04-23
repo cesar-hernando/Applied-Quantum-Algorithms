6e80e6de9a98bc6060ecdf5e3ff6eb5c5c1c2d1f
chernandodelaf <chernandodelaf@tudelft.nl>
2025-04-23 09:29:10 +0200

    recover main and functions file

--- functions.py
+++ functions.py
@@ -4,18 +4,22 @@ In this file, I will define the necessary subroutines for the main file
 
 import numpy as np
 import matplotlib.pyplot as plt
+from typing import Optional
 import pennylane as qml
 
 
-def generate_couplings(n):
+def generate_couplings(n, rng: Optional[np.random.RandomState] = None):
     '''
-    Generate the horizontal and verticals between the qubits in the square lattice
+    Generate the horizontal and vertical couplings between the qubits in the square lattice sampling from a uniform distribution {-1,1}
 
     Inputs
     ------
     n: int
         Number of qubits in each row/column
-    
+
+    rng: numpy.random.mtrand.RandomState
+        Random number generator (for reproducible results)
+
     Outputs
     -------
     J_right: np.ndarray
@@ -26,15 +30,17 @@ def generate_couplings(n):
         Vertical couplings
         Dimension (n-1) x n
     '''
+
+    if rng is None:
+        rng = np.random.RandomState(seed=42)
     
-    rng = np.random.RandomState(seed=42)
     J_right = np.array([[rng.choice([+1,-1]) for j in range(n-1)] for i in range(n)])
     J_down = np.array([[rng.choice([+1,-1]) for j in range(n)] for i in range(n-1)])
 
     return J_right, J_down
 
 
-def hamiltonian(n, qubit_coord, J_right, J_down):
+def hamiltonian(n, J_right, J_down):
     '''
     Computes the Hamiltonian of a 2D antiferromagnetic system of N spins
 
@@ -43,25 +49,21 @@ def hamiltonian(n, qubit_coord, J_right, J_down):
     n: int
         Number of qubits in each row/column (n = sqrt(N))
 
-    qubit_coord: list
-        List with the coordinates (tuples) of all the qubits in the square lattice
-        Dimension n x n
-        
-    J_right: np.ndarray
-        Matrix with the horizontal couplings
-        Dimension (n-1) x n
-
-    J_down: np.ndarray
-        Matrix with the vertical couplings
+     J_right: np.ndarray
+        Horizontal couplings
         Dimension n x (n-1)
 
+    J_down: np.ndarray
+        Vertical couplings
+        Dimension (n-1) x n
+    
     Output
     ------
-    hamiltonian: np.ndarray
-        Matrix of the Hamiltonian in the computational basis
-        Dimension 2**N x 2**N
+    hamiltonian: qml.Hamiltonian
+        Hamiltonian representation of the system for specific coupling coefficients
         
     '''
+
     coef = []
     ops = []
 
@@ -85,6 +87,111 @@ def hamiltonian(n, qubit_coord, J_right, J_down):
 
     return hamiltonian
 
+def observable(obs_name):
+    '''
+    Computes the Hamiltonian of a 2D antiferromagnetic system of N spins
+
+    Inputs
+    ------
+    obs_name: str
+        Name/identification of the observable to measure
+
+    Output
+    ------
+    observable: qml.Hamiltonian
+        Representation of the observable that we want to measure
+        
+    '''
+    if obs_name == 'corr01':
+        # Define the observable to measure: the correlation function between qubits 0 and 1
+        coefs = (1/3)*np.ones(3)
+        ops = [qml.PauliX(0) @ qml.PauliX(1), qml.PauliY(0) @ qml.PauliY(1), qml.PauliZ(0) @ qml.PauliZ(1)]
+        observable = qml.Hamiltonian(coefs, ops)
+    
+    return observable
+
+
+def ground_state_expectation_value(n, hamiltonian, observable):
+    '''
+    Computes the ground state expectation value of a given observable classically
+
+    Inputs
+    ------
+    n: int
+        Number of qubits in each row/column (n = sqrt(N))
+
+    hamiltonian: qml.Hamiltonian
+        Hamiltonian representation of the system for specific coupling coefficients
+
+    observable: qml.Hamiltonian
+        Representation of the k-local observable whose ground state expectation value we want to evaluate
+
+    Output
+    ------
+    expectation_value: float
+        Formula: Tr(\rho(x) O)
+        
+    '''
+     
+    # Transform the qml.Hamiltonian objects to np matrices
+    H_matrix = qml.matrix(hamiltonian)
+    obs_matrix = qml.matrix(observable, wire_order=[i for i in range(n*n)])
+
+    # Diagonalize the Hamiltonian to find the ground state
+    eigenvalues, eigenvectors = np.linalg.eigh(H_matrix)
+    ground_state_energy = eigenvalues[0]
+    ground_state = eigenvectors[:,0]
+
+    # Print the ground state and its energy
+    verbose = False
+    if verbose:
+        np.set_printoptions(precision=1, suppress=True)
+        print(f"Ground state =  {ground_state.real}")
+        print(f"GS energy = {ground_state_energy}")
+
+    # Calculate the expectation value of correlation_01 in the ground state
+    expectation_value = (ground_state.conj().T @ obs_matrix @ ground_state).real
+
+    return expectation_value
+
+def generate_training_set(n, num_examples, observable_name):
+    '''
+    Generate the training set for the model
+
+    Inputs
+    ------
+    n: int
+        Number of qubits in each row/column (n = sqrt(N))
+
+    num_examples: int
+       Number of training examples |{x^l={J_{ij}^l, y^l}|
+
+    observable_name: str
+        Name/identification of the k-local observable whose ground state expectation value we want to evaluate
+
+    Output
+    ------
+    X: np.ndarray
+        Matrix of dimension num_examples x J_len containing num_examples of the input vector
+    
+    Y: np.ndarray
+        Vector of length num_examples with the expectation values of the measured observable
+        
+    ''' 
+
+    rng = np.random.RandomState(seed=42)
+    obs = observable(observable_name)
+    J_len = int(2*n*n - 2*n)  # Number of couplings = len(J_right) + len(J_down)
+    X = np.zeros((num_examples, J_len))
+    Y = np.zeros(num_examples)
+    for l in range(num_examples):
+        J_right, J_down = generate_couplings(n, rng)
+        J_right_flat = J_right.flatten()
+        J_down_flat = J_down.flatten()
+        X[l,:] = np.concatenate((J_right_flat, J_down_flat))
+        Y[l] = ground_state_expectation_value(n, hamiltonian(n, J_right, J_down), obs)
+
+    return X, Y
 
 
 
--- main.py
+++ main.py
@@ -8,33 +8,38 @@ In this file, we train a Machine Learning (ML) model to learn to predict propert
 import numpy as np
 import matplotlib.pyplot as plt
 import pennylane as qml
-import classes
+import functions
 
-# Set the number of qubits in each row/column of the square grid
-n = 2
+mode = 'training'
 
-# Create an object of the Antiferromagnetic_2D class and compute the Hamiltonian of the system
-antiferromagnetic_2D = classes.Antiferromagnetic_2D(n)
-H = antiferromagnetic_2D.hamiltonian()
-H_matrix = qml.matrix(H)
+if mode == 'basic_test':
+    # Set the number of qubits in each row/column of the square grid
+    n = 2
 
-# Diagonalize the Hamiltonian to find the ground state
-eigenvalues, eigenvectors = np.linalg.eigh(H_matrix)
-ground_state_energy = eigenvalues[0]
-ground_state = eigenvectors[:,0]
-np.set_printoptions(precision=1, suppress=True)
-print(f"Ground state =  {ground_state.real}")
-print(f"GS energy = {ground_state_energy}")
+    # Generate the coupling coefficients
+    J_right, J_down = functions.generate_couplings(n)
 
-# Define the observable to measure: the correlation function between qubits 0 and 1
-coefs = (1/3)*np.ones(3)
-ops = [qml.PauliX(0) @ qml.PauliX(1), qml.PauliY(0) @ qml.PauliY(1), qml.PauliZ(0) @ qml.PauliZ(1)]
-correlation_01 = qml.Hamiltonian(coefs, ops)
-correlation_01_matrix = qml.matrix(correlation_01, wire_order=[i for i in range(n*n)])
+    # Obtain the Hamiltonian (qml.Hamiltonian) of the 2D Antiferromagnetic lattice
+    H = functions.hamiltonian(n, J_right, J_down)
 
-# Calculate the expectation value of correlation_01 in the ground state
-exp_val_corr_01 = (ground_state.conj().T @ correlation_01_matrix @ ground_state).real
-print(f"Expectation value of the correlation of qubits 0 and 1 = {exp_val_corr_01}")
+    # Define the observable
+    correlation_01 = functions.observable('corr01')
+
+    # Calculate the ground state expectation value of the observable
+    exp_val_corr_01 = functions.ground_state_expectation_value(n, H, correlation_01)
+    print(f"Expectation value of the correlation of qubits 0 and 1 = {exp_val_corr_01}")
+
+elif mode == 'training':
+    n = 2
+    num_examples = 1000
+    obs_name = 'corr01'
+
+    X, Y = functions.generate_training_set(n, num_examples, obs_name)
+    plt.figure()
+    plt.plot(range(len(Y)), Y)
+    plt.xlabel('Coupling random realization')
+    plt.ylabel(f'Expectarion value of {obs_name}')
+    plt.show()
 
 
 
